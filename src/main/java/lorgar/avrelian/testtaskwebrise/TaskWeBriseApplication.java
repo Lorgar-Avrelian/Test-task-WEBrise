13b7a72282716920422f704d04c31f2c05b96742
lorgar-avrelian <victor-14-244@mail.ru>
2025-05-06 21:33:26 +0300

    Project initialization

---
+++ src/main/java/lorgar/avrelian/testtaskwebrise/TaskWeBriseApplication.java
@@ -0,0 +1,13 @@
+package lorgar.avrelian.testtaskwebrise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskWeBriseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskWeBriseApplication.class, args);
+    }
+
+}
